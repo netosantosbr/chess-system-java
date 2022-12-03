f1a1edf3635dc3d6479aeca926cd1dfc20d3bb54
José Alfredo <netoalfredo9@gmail.com>
2022-12-03 20:33:59 -0300

    Improvements

--- src/application/Application.java
+++ src/application/Application.java
@@ -40,7 +40,11 @@ public class Application {
 				
 				if(chessMatch.getPromoted() != null) {
 					System.out.print("Enter piece for promotion(B/N/R/Q): ");
-					String type = sc.nextLine();
+					String type = sc.nextLine().toUpperCase();
+					while(!type.equals("B") && !type.equals("Q") && !type.equals("R") && !type.equals("N")) {
+						System.out.println("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+					}
 					chessMatch.replacePromotedPiece(type);
 				}
 			} catch (ChessException e) {
